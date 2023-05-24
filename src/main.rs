c224ca909c84da7aa46a166c2d73972caa591c7a
modotte <runecroon@gmail.com>
2023-05-24 19:58:42 +0800

    Clean

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+use notify_rust::Notification;
 use octocrab::Octocrab;
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
-    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+    let token = std::env::var("GITHUB_NOTIFY_KEY").expect("GITHUB_TOKEN env variable is required");
     let octo = Octocrab::builder().personal_token(token).build()?;
-    let thread = octocrab::instance()
-        .activity()
-        .notifications()
-        .list()
-        .send()
-        .await?;
+    let current_rate_limit = octo.ratelimit().get().await?;
+    if current_rate_limit.rate.remaining <= 2 {
+        eprintln!("Cannot request more from Github API!");
+    }
 
-    println!("{:?}", thread.items);
+    let notifications = octo.activity().notifications().list().send().await?;
+
+    println!("{:?}", notifications.items);
 
     Ok(())
 }
