631373a765faf9c493ffe4e6fc26557fc3c5fb39
modotte <runecroon@gmail.com>
2023-05-24 20:22:06 +0800

    Add daemonize

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
+use daemonize::Daemonize;
 use notify_rust::Notification;
 use octocrab::Octocrab;
+use std::fs::File;
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
     let token = std::env::var("GITHUB_NOTIFY_KEY").expect("GITHUB_TOKEN env variable is required");
     let octo = Octocrab::builder().personal_token(token).build()?;
-    let current_rate_limit = octo.ratelimit().get().await?;
-    if current_rate_limit.rate.remaining <= 2 {
-        eprintln!("Cannot request more from Github API!");
+
+    loop {
+        let current_rate_limit = octo.ratelimit().get().await?;
+        if current_rate_limit.rate.remaining <= 2 {
+            eprintln!("Cannot request more from Github API!");
+        }
+
+        let notification = octo.activity().notifications().list().send().await?;
+
+        if notification.items.len() > 0 {
+            Notification::new()
+                .summary("New Github notification!")
+                .body(notification.items[0].reason.as_str())
+                .icon("github")
+                .show()
+                .expect("Failed to launch notification!");
+        }
+        break Ok(());
     }
-
-    let notifications = octo.activity().notifications().list().send().await?;
-
-    println!("{:?}", notifications.items);
-
-    Ok(())
 }
