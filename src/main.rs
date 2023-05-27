ad82b6ca39721b346414e99b3166f605a4bf5cf2
modotte <runecroon@gmail.com>
2023-05-27 23:24:05 +0800

    Clean

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use chrono::{DateTime, Local};
 use notify_rust::Notification;
 use octocrab::Octocrab;
+use std::env::var;
 use std::{thread, time::Duration};
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
-    let token = std::env::var("GITHUB_NOTIFY_KEY").expect("GITHUB_TOKEN env variable is required");
+    let token = var("GITHUB_NOTIFY_KEY").expect("GITHUB_NOTIFY_KEY env variable is required");
     let octo = Octocrab::builder().personal_token(token).build()?;
 
     loop {
         thread::sleep(Duration::from_secs(60 * 10));
         let now: DateTime<Local> = Local::now();
-        println!("Querying Github API at {}", now);
+        println!("github_notify: Querying Github API at {}", now);
 
         let current_rate_limit = octo.ratelimit().get().await?;
         if current_rate_limit.rate.remaining <= 2 {
-            eprintln!("Cannot request more from Github API!");
+            eprintln!("github_notify: Cannot request more from Github API!");
         }
 
         let notification = octo.activity().notifications().list().send().await?;
